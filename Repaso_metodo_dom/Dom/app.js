96484db73b30e2a587d0bf27a32a323829fd56f2
Diego Hernandez <hernandezroadiegoandres@gmail.com>
2024-06-18 21:15:09 -0500

    Dom

---
+++ Repaso_metodo_dom/Dom/app.js
@@ -0,0 +1,12 @@
+
+//objeto padre navegador
+
+let wh = window;
+let dom = document;
+let head = dom.head;
+let body = dom.body;
+
+console.log(wh);
+console.log(dom);
+console.log(head);
+console.log(body);
